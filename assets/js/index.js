b39196f0cb5cadd3d247d9238821088e0c4206ac
Oscar Vega <os_ar1@hotmail.com>
2021-01-24 13:07:04 -0600

    Cambio de font-family, add tooltips, y color de letras

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,10 +5,13 @@ $(document).ready(function(){
         var hash = this.hash;
         $('html, body').animate({
           scrollTop: $(hash).offset().top
-        },1000,function(){
+        },2000,function(){
           window.location.hash = hash
         })
       }
+    });
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip()
     })
   })
   
